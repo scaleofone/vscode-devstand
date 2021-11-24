48fab7dc39ab191a9d7e9d576614e3fa10724049
Max Pshenichnikov <max@codecomrade.net>
2021-11-24 16:33:09 +0300

    wizard controls

--- src/webview/wizard/Wizard.svelte
+++ src/webview/wizard/Wizard.svelte
@@ -2,19 +2,54 @@
     import '../vscode.css';
     import './wizard.css';
     
-    let counter = 10;
+    let htmlContent = "<html>\n  hello world\n</html>";
+    let cssContent = ".red {\n  color: red;\n}";
+    
+    const pasteCssLinkIfNecessary = () => {
+        const cssLink = "\n  <link href=style.css rel=stylesheet>";
+        if (cssContent.trim()) {
+            if (htmlContent.indexOf(cssLink) == -1) {
+                htmlContent = htmlContent.replace('<html>', "<html>"+cssLink)
+            }
+        } else {
+            htmlContent = htmlContent.replace(cssLink, '')
+        }
+    }
+    const emitFiles = () => {
+        console.log('====== emitFiles ======')
+        console.info('index.html')
+        console.log(htmlContent)
+        if (cssContent.trim()) {
+            console.info('style.css')
+            console.log(cssContent)
+        }
+    }
 
-    const increment = (event) => {
-        counter++
+    const runWizard = () => {
+        pasteCssLinkIfNecessary()
+        emitFiles()
     }
-    const decrement = (event) => {
-        counter--
-    }
+
 </script>
 
 <div>
-    Wizard counter: 
-    <pre class="d-inline-block">{ counter }</pre> 
-    <button class="d-inline-block w-auto" on:click={decrement}> - </button> 
-    <button class="d-inline-block w-auto" on:click={increment}> + </button>
+    index.html
+    <textarea
+        bind:value={htmlContent}
+        cols="30" rows="10" spellcheck="false"
+        class="snippet-textarea"
+    ></textarea>
+
+    <br>
+
+    style.css
+    <textarea
+        bind:value={cssContent}
+        cols="30" rows="10" spellcheck="false"
+        class="snippet-textarea"
+    ></textarea>
+
+    <br>
+
+    <button on:click={runWizard}>Run wizard</button> 
 </div>
