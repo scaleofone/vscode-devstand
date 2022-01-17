86cded957dc25c1697478adf01687c8380c3eb0a
Max Pshenichnikov <max@codecomrade.net>
2022-01-17 20:20:39 +0300

    fixup

--- src/extension/breadboard/addTemplateImport.ts
+++ src/extension/breadboard/addTemplateImport.ts
@@ -8,21 +8,23 @@ export default async function (document: vscode.TextDocument, payload: AddTempla
     const parsed = parser.parse(document.uri.path, text)
     const localBindNodes = parser.getLocalBindNodes(parsed)
     const lastLocalBindNode = localBindNodes.length > 0 ? localBindNodes[localBindNodes.length-1] : undefined
-    let beginLine = 0, beginColumn = 0, endLine = 0, endColumn = 0
+
+    let insertLine = 0, insertColumn = 0
     if (lastLocalBindNode) {
-        endLine = beginLine = lastLocalBindNode.loc.end.line -1
-        endColumn = beginColumn = lastLocalBindNode.loc.end.column // do not -1 because the next char is semi-colon (;)
+        insertLine = lastLocalBindNode.loc.end.line -1
+        insertColumn = lastLocalBindNode.loc.end.column -1
+        if (';' == document.getText(new vscode.Range(insertLine, insertColumn, insertLine, insertColumn+1))) {
+            insertColumn += 1
+        }
     }
     const insertText = `\nlocal ${ payload.variableName } = (import '${ payload.targetFile }').${ payload.targetIdentifier };`
 
     const edit = new vscode.WorkspaceEdit()
-    edit.replace(
+    edit.insert(
         document.uri,
-        new vscode.Range(
-            beginLine,
-            beginColumn,
-            endLine,
-            endColumn
+        new vscode.Position(
+            insertLine,
+            insertColumn
         ),
         insertText
     )
