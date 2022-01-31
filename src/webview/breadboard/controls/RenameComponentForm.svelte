e071b8f39c32503cb372b4d378cb51897bb22991
Max Pshenichnikov <max@codecomrade.net>
2022-01-31 19:10:35 +0300

    validation while actionRenameComponent

--- src/webview/breadboard/controls/RenameComponentForm.svelte
+++ src/webview/breadboard/controls/RenameComponentForm.svelte
@@ -2,13 +2,34 @@
     import { onMount, createEventDispatcher } from 'svelte'
     const dispatch = createEventDispatcher()
 
+    import { components } from '../stores/breadboard'
+
+    import { form, field } from 'svelte-forms'
+    import { required } from 'svelte-forms/validators'
+
     export let value = ''
 
     let textInput
 
+    const componentIdentifier = field('componentIdentifier', value, [
+        required(),
+        (val) => ({
+            name: 'unique',
+            valid: (value == val || $components.findIndex(component => component.identifier == val) == -1)
+        }),
+    ])
+    const formInstance = form(componentIdentifier)
+
     function captureEnterAndEscape(event) {
-        if (event.keyCode == 13 /* Enter */) dispatch('success', { value })
-        if (event.keyCode == 27 /* Esc */) dispatch('cancel')
+        if (event.keyCode == 13 /* Enter */) {
+            if (! $formInstance.valid) { return }
+            if ($componentIdentifier.value != value) {
+                dispatch('success', { value: $componentIdentifier.value })
+            } else {
+                dispatch('cancel')
+            }
+        }
+        if (event.keyCode == 27 /* Esc */) { dispatch('cancel') }
     }
 
     onMount(() => textInput.focus())
@@ -17,7 +38,10 @@
 <div>
     <input type="text"
         bind:this={textInput}
-        bind:value={value}
+        bind:value={$componentIdentifier.value}
         on:keydown={captureEnterAndEscape}
     >
+    {#if ! $formInstance.valid}
+        <span style="color:red">{$formInstance.errors[0]}</span>
+    {/if}
 </div>
