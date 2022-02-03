3e5b93adec949248c13d8f20a373aedfa6f3ac52
Max Pshenichnikov <max@codecomrade.net>
2022-02-03 18:58:39 +0300

    position dropdown left if close to window edge

--- src/webview/breadboard/controls/DetailDropdown.ts
+++ src/webview/breadboard/controls/DetailDropdown.ts
@@ -14,11 +14,33 @@ export default function(detailsElement: HTMLDetailsElement) {
         ]('click', handleClickOutside, true)
     }
 
+    let recentClickClientX: number
+    const rememberRecentClick = (event: MouseEvent) => {
+        recentClickClientX = event.clientX
+    }
+
+    const changeOrientation = (event: Event) => {
+        if ((event.target as HTMLDetailsElement).open) {
+            let element: HTMLElement = (event.target as HTMLDetailsElement).querySelector('summary + *')
+            if (window.innerWidth - recentClickClientX < 200) {
+                element.style.left = 'unset'
+                element.style.right = '0'
+            } else {
+                element.style.right = 'unset'
+                element.style.left = '0'
+            }
+        }
+    }
+
     detailsElement.addEventListener('toggle', toggleClickOutsideListener)
+    detailsElement.addEventListener('click', rememberRecentClick)
+    detailsElement.addEventListener('toggle', changeOrientation)
 
     return {
         destroy() {
             detailsElement.removeEventListener('toggle', toggleClickOutsideListener)
+            detailsElement.removeEventListener('click', rememberRecentClick)
+            detailsElement.removeEventListener('toggle', changeOrientation)
             document.removeEventListener('click', handleClickOutside, true)
         }
     };
