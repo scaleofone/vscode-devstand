f6d1637ee2ccfab44619e4a56f20fe20841a68bb
Max Pshenichnikov <max@codecomrade.net>
2022-04-03 23:05:13 +0300

    dummy templates

--- src/breadboard/extension/createNewComponent.ts
+++ src/breadboard/extension/createNewComponent.ts
@@ -1,23 +1,38 @@
 import vscode from 'vscode'
 import findAvailableTemplates from './findAvailableTemplates'
-import { TemplateImport, Component } from '../../BreadboardTypes'
+import { TemplateImport, Component } from '../BreadboardTypes'
 
 export default async function(): Promise<{ templateImport:TemplateImport, component:Component }> {
     let templates = await findAvailableTemplates()
+    let postgres1 = templates.shift()
+    let postgres2 = templates.shift()
+    postgres1.schema.title = 'PostgreSQL database'
+    templates.push(postgres1)
+
     let quickPickItems = templates.map((template, index) => {
         return {
             index,
-            label: template.targetIdentifier,
-            description: template.schema.title,
-            detail: template.schema?.description,
+            label: template.schema.title,
+            // label: template.targetIdentifier,
+            // description: template.schema.title,
+            // detail: template.schema?.description,
         }
     })
+    let dummyTemplates = [
+        'MySQL database',
+        'Redis database',
+        'S3 files storage',
+        'SMTP relay',
+    ]
+    dummyTemplates.forEach((label, i) => quickPickItems.push({ label, index: i+templates.length }))
+
     let selectedQuickPickItem = await vscode.window.showQuickPick(quickPickItems, { title: 'Select template' })
     const selectedTemplate = templates[selectedQuickPickItem.index]
+    let variableName = selectedTemplate.targetIdentifier.startsWith('Postgres') ? 'Postgres' : selectedTemplate.targetIdentifier
     let templateImport = {
         targetFile: selectedTemplate.targetFile,
         targetIdentifier: selectedTemplate.targetIdentifier,
-        variableName: selectedTemplate.targetIdentifier,
+        variableName,
     }
     let selectedComponentName = await vscode.window.showInputBox({ title: 'Name of the square' })
     // TODO validate componentIdentifier is already present
